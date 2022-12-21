d8e26825a494a01f813c3cd80bf5547d99e477fb
dm0114 <dm10802@gmail.com>
2022-12-22 00:24:50 +0900

    [03] refactor: DOM API 제거 및 리펙토링

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-
+// 1. ajax와 xhr & fetch
 // https://junhobaik.github.io/ajax-xhr-fetch/
 // https://velog.io/@dasssseul/JS-%EB%B9%84%EB%8F%99%EA%B8%B0-%ED%86%B5%EC%8B%A0-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0XHR%EA%B3%BC-Fetch-API
 
+// 2. 이벤트
 // event - 앱 애플리케이션의 화면을 어떻게 전환할 것인가 : "이벤트"
 // 브라우저에서 제공하는 API로 해당 UI에 대한 함수를 호출해준다.
+
+// 3. DOM API의 한계 - 명령형
+// DOM API는 UI의 구조가 잘 드러나지 않는다.
+// 해결 방법 - DOM API를 사용하지 않는다..? => 문자열로 만든다.
 
 const container = document.getElementById("root");
 const content = document.createElement('div');
@@ -11,30 +16,42 @@ const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = (url) => `https://api.hnpwa.com/v0/item/${url}.json`;
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+const getData = (url) => {
+  ajax.open('GET', url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
 
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL)
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function() {
-  ajax.open('GET', CONTENT_URL(location.hash.substr(1)), false);
-  ajax.send();
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL(location.hash.substr(1)));
   const title = document.createElement('h1');
 
   title.innerHTML = newsContent.title;
-
   content.appendChild(title);
 })
 
 newsFeed.forEach((item) => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = `#${item.id}`;
-  a.innerHTML = `${item.title} (${item.comments_count})`;
-  ul.appendChild(li);
-  li.appendChild(a);
+  const div = document.createElement("div");
+  
+  // const li = document.createElement("li");
+  // const a = document.createElement("a");
+  // a.href = `#${item.id}`;
+  // a.innerHTML = `${item.title} (${item.comments_count})`;
+  // ul.appendChild(li);
+  // li.appendChild(a);
+
+  div.innerHTML = `
+    <li>
+      <a href="#${item.id}">
+        ${item.title} (${item.comments_count})
+      </a>
+    </li>
+  `;
+
+  ul.appendChild(div.firstElementChild)
 })
 
 
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,11 +118,17 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+// 1. ajax와 xhr & fetch
 // https://junhobaik.github.io/ajax-xhr-fetch/
 // https://velog.io/@dasssseul/JS-%EB%B9%84%EB%8F%99%EA%B8%B0-%ED%86%B5%EC%8B%A0-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0XHR%EA%B3%BC-Fetch-API
 
+// 2. 이벤트
 // event - 앱 애플리케이션의 화면을 어떻게 전환할 것인가 : "이벤트"
 // 브라우저에서 제공하는 API로 해당 UI에 대한 함수를 호출해준다.
+
+// 3. DOM API의 한계 - 명령형
+// DOM API는 UI의 구조가 잘 드러나지 않는다.
+// 해결 방법 - DOM API를 사용하지 않는다..? => 문자열로 만든다.
 
 var container = document.getElementById("root");
 var content = document.createElement('div');
@@ -131,25 +137,31 @@ var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = function CONTENT_URL(url) {
   return "https://api.hnpwa.com/v0/item/".concat(url, ".json");
 };
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
+var getData = function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+};
+var newsFeed = getData(NEWS_URL);
 var ul = document.createElement('ul');
 window.addEventListener('hashchange', function () {
-  ajax.open('GET', CONTENT_URL(location.hash.substr(1)), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL(location.hash.substr(1)));
   var title = document.createElement('h1');
   title.innerHTML = newsContent.title;
   content.appendChild(title);
 });
 newsFeed.forEach(function (item) {
-  var li = document.createElement("li");
-  var a = document.createElement("a");
-  a.href = "#".concat(item.id);
-  a.innerHTML = "".concat(item.title, " (").concat(item.comments_count, ")");
-  ul.appendChild(li);
-  li.appendChild(a);
+  var div = document.createElement("div");
+
+  // const li = document.createElement("li");
+  // const a = document.createElement("a");
+  // a.href = `#${item.id}`;
+  // a.innerHTML = `${item.title} (${item.comments_count})`;
+  // ul.appendChild(li);
+  // li.appendChild(a);
+
+  div.innerHTML = "\n    <li>\n      <a href=\"#".concat(item.id, "\">\n        ").concat(item.title, " (").concat(item.comments_count, ")\n      </a>\n    </li>\n  ");
+  ul.appendChild(div.firstElementChild);
 });
 container.appendChild(ul);
 container.appendChild(content);
@@ -178,7 +190,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51296" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64609" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
